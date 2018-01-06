a28fe3ae465ec97205bea04b90c4283a43f574b3
zcochran <fuzzylimes@gmail.com>
2018-01-06 10:55:07 -0500

    Added some 'UnitTests'
    Basic santiy tests for positive and negative scenarios.

---
+++ hltbScraper/tests/test_hltb.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from hltbScraper import scraper as hltb
+
+class TestScraper(TestCase):
+    def test_HLTB(self):
+        title = 'Final Fantasy'
+        a = hltb.HLTB(title)
+        self.assertTrue(isinstance(a,dict))
+        self.assertTrue(title in a.keys())
+        self.assertTrue('url' in a[title].keys())
+        self.assertTrue('Main Story' in a[title].keys())
+        self.assertTrue('Main + Extra' in a[title].keys())
+        self.assertTrue('Completionist' in a[title].keys())
+
+    def test_HLTB_not_found(self):
+        title = 'asfkjasf'
+        a = hltb.HLTB(title)
+        self.assertTrue(isinstance(a, dict))
+        self.assertTrue('error' in a.keys())
+
+    def test_get_page(self):
+        a = hltb.GetPage('Final Fantasy')
+        self.assertTrue(isinstance(a, str))
+
